9e2f3569e6fedfb3e4903f5929cdf5ee43ceaa2c
fahamed1 <fahamed1@fordham.edu>
2024-12-13 21:37:27 -0500

    Added switching scenes to inside level

--- capstone-game/area_3d.gd
+++ capstone-game/area_3d.gd
@@ -3,15 +3,16 @@ extends Area3D
 @export var inside_castle = preload("res://insideCastle.tscn")
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#pass # Replace with function body.
 
+func _on_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:  # Ensure the entering body is the player
+		call_deferred("change_scene")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _on_body_entered(body: Node3D) -> void:
+	#if body is CharacterBody3D: # Replace with function body.
+		#get_tree().change_scene_to_packed(inside_castle)
 
-
-func _on_area_entered(body: Node) -> void:
-	if body is CharacterBody3D: # Replace with function body.
-		get_tree().change_scene(inside_castle)
+func change_scene() -> void:
+	get_tree().change_scene_to_packed(inside_castle)
